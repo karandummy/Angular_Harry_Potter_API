44950147d0f6c46c65b6323a77634676f8d50117
karandummy <karan09022005@gmail.com>
2025-05-17 05:21:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NgFor, FormsModule, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'api';
+  api = inject(ApiService);
+  characters: any[] = [];
+  selectedCharacter: any;
+
+  ngOnInit() {
+    this.api.getData().subscribe((res) => {
+      this.characters = res;
+    });
+  }
+
+  onCharacterChange(event: any) {
+    const selectedName = event.target.value;
+    this.selectedCharacter = this.characters.find(char => char.name === selectedName);
+  }
 }
